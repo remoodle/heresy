39a7319bd6fa5500f89e050ccccbd40741659046
Alex <widesehl@gmail.com>
2024-01-07 18:23:25 +0600

    feat: cdn prefixing (#4)

--- remoodle/frontend/web/vite.config.ts
+++ remoodle/frontend/web/vite.config.ts
@@ -1,10 +1,9 @@
 import { fileURLToPath, URL } from "node:url";
-
+import { extname } from "node:path";
 import { defineConfig, loadEnv, type IndexHtmlTransformResult } from "vite";
 import vue from "@vitejs/plugin-vue";
 import packageJson from "./package.json";
 
-// https://vitejs.dev/config/
 export default defineConfig((config) => {
   const { mode } = config;
 
@@ -12,10 +11,53 @@ export default defineConfig((config) => {
 
   const isCI = !!env.CI;
   const sha = env.COMMIT_SHA;
+  const cdnPrefixUrl = `https://${env.CDN_PREFIX}.remoodle.pages.dev`;
 
   return {
+    experimental: {
+      renderBuiltUrl(
+        filename: string,
+        {
+          hostId,
+          hostType,
+          type,
+        }: {
+          hostId: string;
+          hostType: "js" | "css" | "html";
+          type: "public" | "asset";
+        },
+      ) {
+        if (type === "public") {
+          return `${cdnPrefixUrl}/` + filename;
+        } else if (extname(hostId) === ".js") {
+          return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` };
+        } else {
+          return `${cdnPrefixUrl}/` + filename;
+        }
+      },
+    },
     plugins: [
       vue(),
+      {
+        name: "cdn-prefix",
+        transformIndexHtml() {
+          if (!cdnPrefixUrl) {
+            return;
+          }
+
+          const els: IndexHtmlTransformResult = [];
+
+          els.push({
+            tag: "script",
+            injectTo: "head",
+            children: `window.__toCdnUrl = (filename) => {
+              return '${cdnPrefixUrl}/' + filename;
+            };`,
+          });
+
+          return els;
+        },
+      },
       {
         name: "build-info",
         transformIndexHtml() {
@@ -30,7 +72,7 @@ export default defineConfig((config) => {
           els.push({
             tag: "script",
             injectTo: "body",
-            children: `__BUILD_INFO__ = ${JSON.stringify({
+            children: `window.__BUILD_INFO__ = ${JSON.stringify({
               version: `${version}.${sha.slice(0, 8)}`,
             })}`,
           });
