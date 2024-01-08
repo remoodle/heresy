f0391c91d30c0fc57ac5b6b7225876360c74c632
Alex Sehl <widesehl@gmail.com>
2024-01-08 13:43:15 +0600

    feat: ui kit

--- remoodle/frontend/web/src/pages/home/Page.vue
+++ remoodle/frontend/web/src/pages/home/Page.vue
@@ -1,5 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from "@/shared/ui/button/Button.vue";
+import { Alert, AlertDescription, AlertTitle } from "@/shared/ui/alert";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/shared/ui/alert-dialog";
+import { Badge } from "@/shared/ui/badge";
+</script>
 
 <template>
-  <main>Hello</main>
+  <main>
+    Hello
+
+    <Button> Click </Button>
+    Hello Hello Hello
+
+    <Alert variant="destructive">
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>
+        You can add components to your app using the cli.
+      </AlertDescription>
+    </Alert>
+
+    <AlertDialog>
+      <AlertDialogTrigger>Open</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+
+    <Badge>Badge</Badge>
+
+    <Badge variant="outline"> Outline </Badge>
+
+    <Badge variant="destructive"> Destructive </Badge>
+  </main>
 </template>
