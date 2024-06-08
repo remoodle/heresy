ef7ccf60c36807386b627589faf7d06b628566db
Alex Sehl <widesehl@gmail.com>
2024-06-08 11:44:57 +0500

    fix: change footer

--- src/widgets/footer/ui/Footer.vue
+++ src/widgets/footer/ui/Footer.vue
@@ -22,6 +22,7 @@ const { providerId, availableProviders } = storeToRefs(appStore);
 </script>
 
 <template>
+  1 23123
   <template v-if="slim">
     <footer class="py-6">
       <div class="mx-auto flex w-full items-center justify-center">
