4865cd23f787d87084e0bbe19b025af5db013ca6
Alex Sehl <widesehl@gmail.com>
2025-08-14 01:52:50 +0500

    Update
    [ghstack-poisoned]

--- apps/backend/src/services/api/index.ts
+++ apps/backend/src/services/api/index.ts
@@ -2,17 +2,20 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
-import { pinoLogger } from "hono-pino-logger";
+import { requestId } from "hono/request-id";
 import { config } from "../../config";
 import { logger } from "../../library/logger";
 import { applyPrometheus } from "./middleware/prometheus";
 import { applyBullBoard } from "./middleware/bull-board";
 import { versionHandler } from "./middleware/version";
+import { loggerMiddleware } from "./middleware/logger";
 import { v2 } from "./router/v2";
 
 const api = new Hono();
 
-api.use("*", pinoLogger(logger.api), prettyJSON());
+api.use(requestId());
+api.use("*", loggerMiddleware);
+api.use("*", prettyJSON());
 api.use("*", versionHandler);
 api.use("*", cors());
 
