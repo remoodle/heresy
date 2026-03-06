9e2b64236486a35751cf33a4ce1d8bd1165eb394
Nariman Sailaubek <sailaubek.nar@gmail.com>
2026-03-07 04:31:09 +0500

    refactor: schedule

---
+++ .github/scripts/schedule-parser/command.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\RequestInterface;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$token = $argv[1];
+$files = scandir($argv[2]);
+
+$groups = [];
+foreach ($files as $file) {
+    if($file === '.' || $file === '..') {
+        continue;
+    }
+    $parser = new \Smalot\PdfParser\Parser();
+    $pdfFile = $parser->parseFile($argv[2] . '/' . $file);
+    foreach (explode("\n", $pdfFile->getText()) as $line) {
+        if(strpos($line, 'Group ') === 0) {
+            $groups[explode(' ', $line)[1]] = true;
+        }
+    }
+}
+$groups = array_keys($groups);
+
+$client = new Client([
+    'base_uri' => 'https://du.astanait.edu.kz:8765',
+    'headers' => [
+        'Authorization' => 'Bearer ' . trim($token)
+    ]
+]);
+
+$groupScheduleRaw = [];
+
+$requests = array_map(
+    fn (string $groupName): RequestInterface => new Request('GET', '/astanait-schedule-module/api/v1/schedule/groupName/' . $groupName),
+    $groups
+);
+$pool = new Pool(
+    $client,
+    $requests,
+    [
+        'concurrency' => 10,
+        'fulfilled' => function (Response $response, $index) use (&$groupScheduleRaw) {
+            $body = json_decode($response->getBody()->getContents(), true)['body'];
+            if(count($body) > 0) {
+                $groupScheduleRaw[$body[0]['group']] = $body;
+            }
+
+            return true;
+        },
+        'rejected' => function (RequestException $reason, $index) use (&$requests) {
+            $requests[] = $reason->getRequest();
+            echo "\nUnable to fetch " . $reason->getRequest()->getUri()->__toString();
+            return false;
+        },
+    ]
+);
+
+$promise = $pool->promise();
+$promise->wait();
+
+$formattedSchedule = [];
+
+foreach ($groupScheduleRaw as $groupName => $schedule) {
+    $formattedSchedule[$groupName] = [];
+    foreach ($schedule as $lesson) {
+        $timeStart = null;
+        $timeFinish = null;
+
+        $classTimeDay = $lesson['classtime_day'];
+        $daysMetaInfo = json_decode($lesson['days'], true);
+        $dayIndex = array_search($classTimeDay, $daysMetaInfo['days'], true);
+        $weekDay = $daysMetaInfo['daysname'][$dayIndex];
+
+        $classTimeTime = $lesson['classtime_time'];
+        foreach ($daysMetaInfo['time'] as $timeMetaInfo) {
+            if($timeMetaInfo['id'] === $classTimeTime) {
+                $timeStart = $timeMetaInfo['start'];
+                $timeFinish = $timeMetaInfo['finish'];
+            }
+        }
+
+        if($timeStart === null || $timeFinish === null) {
+            continue;
+        }
+
+        $formattedSchedule[$groupName][] = [
+            'id' => uniqid(),
+            'start' => "$weekDay " . $timeStart,
+            'end' => "$weekDay " . $timeFinish,
+            'courseName' => $lesson['subject'],
+            'location' => $lesson['room'],
+            'isOnline' => $lesson['room'] === 'online',
+            'teacher' => $lesson['tutor'],
+            'type' => $lesson['lesson_type'],
+        ];
+    }
+}
+
+file_put_contents($argv[3] . '/output.json', json_encode($formattedSchedule, JSON_PRETTY_PRINT));
